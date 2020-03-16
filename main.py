0aaa155610ada9fa472252cff01c4a180b42a06c
John Amarante <john.amarante8888@gmail.com>
2020-03-16 10:36:09 -0700

    use vairables (untested)

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ for k in range(4):
     subloop = tqdm(total = 2500, position=1, leave=False)
     for m in range(2500):
         subloop.update(1)
-        #sub_description = str(m)/"2500"
-        subloop.set_description(str(m) + "/2500")
+        sub_description = str(m)/"2500"
+        subloop.set_description(sub_description)
     subloop.close()
     loop.update(1)
-    #description = str(k)/"2500"
-    loop.set_description(str(k) + "/4")
+    description = str(k)/"2500"
+    loop.set_description(description)
     time.sleep(1)
 loop.close()
