71cf69dfc67b36accb68af323ed8da48dd626613
John Amarante <john.amarante8888@gmail.com>
2020-03-16 12:27:53 -0700

    OTHER ENHANCEMENT

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import time
+from datetime import datetime
 from tqdm import tqdm
 loop = tqdm(total = 4, position=0, leave=False)
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S %Z")
+print("Start Time = ", current_time)
 for k in range(4):
 
-    #print("substart")
     subloop = tqdm(total = 2500, position=1, leave=False)
     for m in range(2500):
         subloop.update(1)
@@ -15,3 +18,7 @@ for k in range(4):
     loop.set_description(description)
     time.sleep(1)
 loop.close()
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S %Z")
+print("End Time = ", current_time)
